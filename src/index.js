5fea55721a76fd63c205c5f130f020d72379cbb9
ATOROYO <davidatoroyosika@gmail.com>
2025-02-19 19:04:47 +0300

    Changed Pizza component div to li for pizza class

--- src/index.js
+++ src/index.js
@@ -99,14 +99,14 @@ function Menu() {
 function Pizza(props) {
   console.log(props);
   return (
-    <div className="pizza">
+    <li className="pizza">
       <img src={props.pizzaObj.photoName} alt={props.pizzaObj.name} />
       <div>
         <h3>{props.pizzaObj.name}</h3>
         <p>{props.pizzaObj.ingredients}</p>
         <p>{props.pizzaObj.price + 3}</p>
       </div>
-    </div>
+    </li>
   );
 }
 
