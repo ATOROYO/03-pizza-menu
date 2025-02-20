ecfe2119b88526b61ad1dc0150aea19341634253
ATOROYO <davidatoroyosika@gmail.com>
2025-02-20 18:51:51 +0300

    Update Pizza component to display sold-out status and adjust price display

--- src/index.js
+++ src/index.js
@@ -121,12 +121,12 @@ function Pizza({ pizzaObj }) {
   // if (pizzaObj.soldOut) return null;
 
   return (
-    <li className="pizza">
+    <li className={`pizza ${pizzaObj.soldOut ? "sold-out" : ""}`}>
       <img src={pizzaObj.photoName} alt={pizzaObj.name} />
       <div>
         <h3>{pizzaObj.name}</h3>
         <p>{pizzaObj.ingredients}</p>
-        <p>{pizzaObj.price + 3}</p>
+        <span>{pizzaObj.soldOut ? "SOLD OUT" : pizzaObj.price}</span>
       </div>
     </li>
   );
