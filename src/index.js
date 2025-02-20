ba87c64a381ada8f26eaf08207548420715660da
ATOROYO <davidatoroyosika@gmail.com>
2025-02-20 18:38:30 +0300

    Reorganize Menu component to include description above pizza list and improve layout using React Fragment

--- src/index.js
+++ src/index.js
@@ -76,17 +76,18 @@ function Menu() {
     <main className="menu">
       <h2>Our Menu</h2>
 
-      <p>
-        Authentic Italian cuisine, 6 creative dishes to choose from. All from
-        our oven, all organic, all delicious.
-      </p>
-
       {numPizza > 0 ? (
-        <ul className="pizzas">
-          {pizzaData.map((pizza) => (
-            <Pizza pizzaObj={pizza} key={pizza.name} />
-          ))}
-        </ul>
+        <>
+          <p>
+            Authentic Italian cuisine, 6 creative dishes to choose from. All
+            from our oven, all organic, all delicious.
+          </p>
+          <ul className="pizzas">
+            {pizzaData.map((pizza) => (
+              <Pizza pizzaObj={pizza} key={pizza.name} />
+            ))}
+          </ul>
+        </>
       ) : (
         <p>Sorry, we're currently out of stock. We're working on our menu</p>
       )}
